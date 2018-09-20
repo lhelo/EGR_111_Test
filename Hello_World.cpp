7b3ea690f36baa1ed9da6d72eb5c3b46d9802ddb
chengyangwangmc3 <43442467+chengyangwangmc3@users.noreply.github.com>
2018-09-20 09:23:20 -0400

    Upload Hello World

---
+++ Hello_World.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Computers, computers everywhere"; // display a string
+	cout << "\n as far as I can C"; // display another string
+	return 0;
+}
